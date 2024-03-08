e72f60deed98e7ffe8f59f513a1d3ac5c8f7a76f
Flora <laszlo.t.flora@gmail.com>
2024-03-08 23:34:39 +0100

    unique properties

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,22 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, color, borderColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+	${
+    backgroundColor
+      ? `${backgroundColor} ${color} ${borderColor}`
+      : "bg-coral-red  text-white border-coral-red"
+  }`}
+    >
       {label}
-      {iconURL && <img
-        src={iconURL}
-        alt="right arrow icon"
-        className="ml2 w-5 h-5 rounded-full"
-      />}
-  {/*     <img
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="right arrow icon"
+          className="ml2 w-5 h-5 rounded-full"
+        />
+      )}
+      {/*     <img
         src={iconURL}
         alt="right arrow"
         className={`ml2 ${iconURL == "" ? "w-0 h-0" : "w-5 h-5 rounded-full"}`}
