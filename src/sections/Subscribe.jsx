3c05cd4642722de16f9806662a27c36394d86750
Flora <laszlo.t.flora@gmail.com>
2024-03-10 22:25:02 +0100

    Subscribe

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,26 @@
-const Subscribe = () => {
-  return (
-	<div>Subscribe</div>
-  )
-}
+import { useState } from "react";
+import Button from "../components/Button";
 
-export default Subscribe
+const Subscribe = () => {
+  const [email, setEmail] = useState("");
+  return (
+    <section className="max-container flex max-lg:flex-col justify-between items-center gap-11">
+      <h3 className="flex-1 text-4xl font-palanquin">
+        Sign Up for <span className=" text-coral-red">Updates</span> &
+        Newsletter
+      </h3>
+      <div className="flex-1 flex justify-between items-center rounded-full border-slate-600 border-solid border-2 p-2 w-full">
+        <input
+          className="input"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="subscribe@nike.com"
+        />
+        <Button label="Sign up" />
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
